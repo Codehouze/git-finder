06505bc52224d83c05d1b26a9639ef779a0cdb6d
Codehouzem <codehouseinc@gmail.com>
2022-02-21 15:55:44 +0100

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+         <h1 className="text-6xl mb-4">About</h1></div>
+  )
+}
+
+export default About
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
+}
